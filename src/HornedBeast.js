7f0c21e96d8228e41d2452f8cfe73e4ea30015e2
Nicholas <nicholasbrown.us@gmail.com>
2023-03-13 22:35:42 -0500

    Stretch Goal Complete

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,16 +1,23 @@
 // TODO: Bring in the Imports
 import React from 'react';
+import BeastData from './BeastData';
 
 // TODO: Create the Class and have a Render Method
 class HornedBeast extends React.Component {
   render() {
     return (
-      <h2></h2>
-    )
-
+      <article>
+        {BeastData.map(beast => (
+          <div key={beast._id}>
+            <img src={beast.image_url} alt={beast.title} width="300" height="250"/>
+            <h2>{beast.title}</h2>
+            <p>{beast.description}</p>
+          </div>
+        ))}
+      </article>
+    );
   }
 }
-
 
 // TODO: Export the Class for other Files to Import
 export default HornedBeast;
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 // TODO: Bring in the Imports
 import React from 'react';
+import BeastData from './BeastData';
 import HornedBeast from './HornedBeast';
 
 // TODO: Create the Class and have a Render Method
@@ -7,8 +8,6 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <HornedBeast />
-        <HornedBeast />
         <HornedBeast />
       </>
     )
