db8369c1f431c7697f1ffe16344812569ba6a83e
Nicholas <nicholasbrown.us@gmail.com>
2023-03-15 19:28:31 -0500

    Took out Import Modal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import data from './data/data.json';
-import Modal from 'react-bootstrap/Modal';
 import SelectedBeast from './SelectedBeast';
 
 // TODO: Create the Class - will always have a "render" method
